1a47d6effb1e56393aa6a1446969016b125e0425
Satyam Kumar <satyam.kumar@agreeya.net>
2019-03-05 13:57:37 +0530

    wjegfwhejh

---
+++ digital-domain/src/main/java/com/digital/model/BusType.java
@@ -0,0 +1,36 @@
+package com.digital.model;
+/**
+ * @author Satyam Kumar
+ *
+ */
+public class BusType {
+
+	private int id;
+	private String busType;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the busType
+	 */
+	public String getBusType() {
+		return busType;
+	}
+	/**
+	 * @param busType the busType to set
+	 */
+	public void setBusType(String busType) {
+		this.busType = busType;
+	}
+	
+	
+}
